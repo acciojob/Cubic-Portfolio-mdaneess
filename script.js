9305344fe4b5520c787fa7e61f2a653d3c7d5c9d
MOHAMMAD ANEES <94375140+mdaneess@users.noreply.github.com>
2022-07-22 05:20:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 //your code here
+var cube = document.querySelector('.cube');
+var radioGroup = document.querySelector('.radio-group');
+var currentClass = '';
+
+function changeSide() {
+  var checkedRadio = radioGroup.querySelector(':checked');
+  var showClass = 'show-' + checkedRadio.value;
+  if ( currentClass ) {
+    cube.classList.remove( currentClass );
+  }
+  cube.classList.add( showClass );
+  currentClass = showClass;
+}
+// set initial side
+changeSide();
+
+radioGroup.addEventListener( 'change', changeSide );
 
 function reset() {
     for (i = 1; i <= 9; i++) {
